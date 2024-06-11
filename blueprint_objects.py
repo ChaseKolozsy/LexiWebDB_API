b2a837f62cd0d84bfad64273ce7a48c44ee02e5d
Chase Kolozsy <chasekolozsy@gmail.com>
2024-06-11 15:23:28 -0600

    adds endpooints in blueprint_objects.py to query by association and remove_association.

--- blueprint_objects.py
+++ blueprint_objects.py
@@ -97,3 +97,62 @@ def add_association(object_id):
 
     db.session.commit()
     return jsonify(obj.to_dict()), 200
+
+@objects.route('/query_by_association', methods=['POST'])
+def query_by_association():
+    data = request.get_json()
+    assoc_type = data.get('type')
+    assoc_id = data.get('id')
+
+    if assoc_type == 'verb':
+        objects = Object.query.join(Object.verbs).filter(Verb.verb_id == assoc_id).all()
+    elif assoc_type == 'attribute':
+        objects = Object.query.join(Object.attributes).filter(Attribute.attribute_id == assoc_id).all()
+    elif assoc_type == 'state':
+        objects = Object.query.join(Object.states).filter(State.state_id == assoc_id).all()
+    elif assoc_type == 'routine':
+        objects = Object.query.join(Object.routines).filter(Routine.routine_id == assoc_id).all()
+    else:
+        return jsonify({'error': 'Invalid association type'}), 400
+
+    return jsonify([obj.to_dict() for obj in objects]), 200
+
+@objects.route('/<int:object_id>/remove_association', methods=['POST'])
+def remove_association(object_id):
+    data = request.get_json()
+    obj = Object.query_by_id(object_id)
+    if not obj:
+        return jsonify({'error': 'Object not found'}), 404
+
+    assoc_type = data.get('type')
+    assoc_id = data.get('id')
+
+    if assoc_type == 'verb':
+        assoc_obj = Verb.query_by_id(assoc_id)
+        if assoc_obj and assoc_obj in obj.verbs:
+            obj.verbs.remove(assoc_obj)
+        else:
+            return jsonify({'error': 'Verb not found or not associated'}), 404
+    elif assoc_type == 'attribute':
+        assoc_obj = Attribute.query_by_id(assoc_id)
+        if assoc_obj and assoc_obj in obj.attributes:
+            obj.attributes.remove(assoc_obj)
+        else:
+            return jsonify({'error': 'Attribute not found or not associated'}), 404
+    elif assoc_type == 'state':
+        assoc_obj = State.query_by_id(assoc_id)
+        if assoc_obj and assoc_obj in obj.states:
+            obj.states.remove(assoc_obj)
+        else:
+            return jsonify({'error': 'State not found or not associated'}), 404
+    elif assoc_type == 'routine':
+        assoc_obj = Routine.query_by_id(assoc_id)
+        if assoc_obj and assoc_obj in obj.routines:
+            obj.routines.remove(assoc_obj)
+        else:
+            return jsonify({'error': 'Routine not found or not associated'}), 404
+    else:
+        return jsonify({'error': 'Invalid association type'}), 400
+
+    db.session.commit()
+    return jsonify(obj.to_dict()), 200
