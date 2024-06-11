e055b6106c101272aedef21e3a170b6cb7e9ab36
Chase Kolozsy <chasekolozsy@gmail.com>
2024-06-11 15:14:36 -0600

    adds return statement to blueprint_objects.py's get_all_objects()

--- blueprint_objects.py
+++ blueprint_objects.py
@@ -14,6 +14,7 @@ def get_objects_schema():
 @objects.route('', methods=['GET'])
 def get_all_objects():
     objects = Object.query_all()
+    return jsonify([obj.to_dict() for obj in objects]), 200
 
 @objects.route('/<int:object_id>', methods=['GET'])
 def get_object(object_id):
